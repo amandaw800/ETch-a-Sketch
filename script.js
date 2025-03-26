ef47137df0466dfe4602b07087d576ee36a8ecb3
amandaw800 <amanda.v.wei@gmail.com>
2025-03-26 15:47:29 -0400

    draft of etch a sketch

--- script.js
+++ script.js
@@ -2,38 +2,68 @@ const body = document.querySelector("body");
 // Create a container element that's a div that will contain all the other divs
 const container = document.createElement("div"); 
 const squareContainer = document.createElement("div");
+const changeButton = document.querySelector(".change");
 
 //Create a div that is a square
 const square = document.createElement("div");
 
-container.style.borderStyle = "solid";
-container.style.borderColor = "red";
+
 container.style.width = "2059px";
-
-squareContainer.style.borderStyle = "solid";
-squareContainer.style.borderColor = "orange"
-
 
 
 
 squareContainer.style.display = "flex";
 squareContainer.style.flexWrap = "wrap";
 
+changeButton.addEventListener("click", ()=>
+{
+    let grid = parseInt(prompt("How many squares per side should there be?"))
+
+    removeSquares();
+    
+    container.style.width = (grid * 125 + 59) + 'px';
+
+    makeSquares(grid);
+    setTimeout(hover, 0);
+
+    hover();
 
 
+    //makeSquares(grid); // array of all the squares
+
+}
+);
+
+
+function hover()
+{
+    const squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) =>
+    {
+    
+    square.addEventListener("mouseover", ()=>
+    {
+        
+        square.style.backgroundColor = "red";
+    });
+});
+
+}
 
 
 //Creates squares and stores them into an array
-function makeSquares()
+function makeSquares(n)
 {
     let arr = []
 
-    for(let i = 0; i < 16*16; i++)
+    for(let i = 0; i < n*n; i++)
     {
         const square = document.createElement("div");
         square.style.borderStyle = "solid";
         square.style.width = "125px";
         square.style.height = "125px";
+        square.classList.add("square");
 
         arr.push(square);
 
@@ -41,14 +71,25 @@ function makeSquares()
         squareContainer.appendChild(square);
 
     }
+
     return arr;
 
 }
 
+function removeSquares(){
+    const item = document.getElementById("squareContainer");
+    squareContainer.innerHTML = '';
+    
 
-//Call functions
-makeSquares();
+}
 
+
+
+
+makeSquares(16);
+setTimeout(hover, 0);
+
+hover();
 
 
 container.appendChild(squareContainer)
